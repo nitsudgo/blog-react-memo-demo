0da08accf4a1f0805c29a57b866d7fb9422ffc9c
hiddenbiscuit <hiddenbiscuit.dev@gmail.com>
2022-07-09 15:01:35 +0800

    Add commentsd

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import { memo, useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  // "value" will be passed down to our child components
   let [value, setValue] = useState(0);
+
+  // "valueX" is not used in our child components at all- we will use it to force this App component to re-render
   let [valueX, setValueX] = useState(0);
   
   return (
@@ -41,6 +44,8 @@ function App() {
 
 export default App;
 
+// A non-memoized component. Whenever the name or value props change, it will re-render.
+// Whenever the parent component (in this case, App) re-renders, this will also re-render.
 const NoMemoComponent = ({ name, value }) => {
   useEffect(() => {
     console.log(`Component ${name} has updated!`);
@@ -55,4 +60,8 @@ const NoMemoComponent = ({ name, value }) => {
   );
 }
 
+// All we had to do to memoize the component was to pass it to a memo() function.
+// The resultant component only re-renders if the props it consumes change.
+// It will not re-render if the props are the same- even if the parent component re-rendered!
+// This function also takes a second argument where we can specify the comparison method to check for prop changes.
 const YesMemoComponent = memo(NoMemoComponent);
